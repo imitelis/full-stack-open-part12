5af6e978463813919fe359caec6f850ba23f4f26
imitelis <dsaavedra@unal.edu.co>
2025-07-01 23:59:06 -0500

    exs 12.21.-12.22.

--- todo-app/todo-frontend/src/tests/Todo.test.js
+++ todo-app/todo-frontend/src/tests/Todo.test.js
@@ -12,30 +12,30 @@ describe('Todo component', () => {
     done: false,
   };
 
-  test('renders todo text', () => {
+  test('Renders todo text', () => {
     render(<Todo todo={todo} deleteTodo={mockDelete} completeTodo={mockComplete} />);
     expect(screen.getByText('Write tests')).toBeInTheDocument();
   });
 
-  test('renders not done info when todo is not done', () => {
+  test('Renders not done info when todo is not done', () => {
     render(<Todo todo={todo} deleteTodo={mockDelete} completeTodo={mockComplete} />);
     expect(screen.getByText(/This todo is not done/)).toBeInTheDocument();
     expect(screen.getByText(/Set as done/)).toBeInTheDocument();
   });
 
-  test('calls deleteTodo when Delete is clicked', () => {
+  test('Calls deleteTodo when Delete is clicked', () => {
     render(<Todo todo={todo} deleteTodo={mockDelete} completeTodo={mockComplete} />);
     fireEvent.click(screen.getByText('Delete'));
     expect(mockDelete).toHaveBeenCalledWith(todo);
   });
 
-  test('calls completeTodo when Set as done is clicked', () => {
+  test('Calls completeTodo when Set as done is clicked', () => {
     render(<Todo todo={todo} deleteTodo={mockDelete} completeTodo={mockComplete} />);
     fireEvent.click(screen.getByText('Set as done'));
     expect(mockComplete).toHaveBeenCalledWith(todo);
   });
 
-  test('renders done info when todo is done', () => {
+  test('Renders done info when todo is done', () => {
     const doneTodo = { ...todo, done: true };
     render(<Todo todo={doneTodo} deleteTodo={mockDelete} completeTodo={mockComplete} />);
     expect(screen.getByText(/This todo is done/)).toBeInTheDocument();
